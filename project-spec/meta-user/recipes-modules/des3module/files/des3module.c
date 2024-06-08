64e62c61e4e5dcae7ebe98af106682ac3d4d22be
lorenzobadas <lorenzo.badas@gmail.com>
2024-06-08 10:55:58 +0200

    undefine DEBUG in driver

--- project-spec/meta-user/recipes-modules/des3module/files/des3module.c
+++ project-spec/meta-user/recipes-modules/des3module/files/des3module.c
@@ -58,8 +58,6 @@
 #define SUCCESS 0
 #define DATA_SIZE 8
 
-#define DEBUG
-
 /* Standard module information, edit as appropriate */
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR
@@ -73,7 +71,7 @@ static struct class *cls;
 /* This is called whenever a process attempts to open the device file */ 
 static int device_open(struct inode *inode, struct file *file) 
 { 
-	#ifdef DEBUG
+	#ifdef DEBUG_DES3MODULE
     pr_info("device_open(%p)\n", file); 
 	#endif
  
@@ -83,7 +81,7 @@ static int device_open(struct inode *inode, struct file *file)
 
 static int device_release(struct inode *inode, struct file *file) 
 { 
-	#ifdef DEBUG
+	#ifdef DEBUG_DES3MODULE
     pr_info("device_release(%p,%p)\n", inode, file); 
 	#endif
  
@@ -101,7 +99,7 @@ static ssize_t device_read(struct file *file, /* see include/linux/fs.h   */
 { 	
 	int i;
 	unsigned int result[13];
-	#ifdef DEBUG
+	#ifdef DEBUG_DES3MODULE
 	pr_info("Starting read from AXI Registers\n");
 	#endif
 	result[ 0] = ioread32(axi_slv_regs + DES3_AXI_DES_IN_HI_OFFSET);
@@ -117,7 +115,7 @@ static ssize_t device_read(struct file *file, /* see include/linux/fs.h   */
 	result[10] = ioread32(axi_slv_regs + DES3_AXI_DES_OUT_LO_OFFSET);
 	result[11] = ioread32(axi_slv_regs + DES3_AXI_READY_OFFSET);
 	result[12] = ioread32(axi_slv_regs + DES3_AXI_VALID_KEY_OFFSET);
-	#ifdef DEBUG
+	#ifdef DEBUG_DES3MODULE
 	pr_info("Read from AXI Registers completed\n");
 	pr_info("des_in  : 0x%08X%08X\n", result[0], result[1]);
 	pr_info("decrypt : 0x%08X\n", 	  result[8]);
@@ -139,7 +137,7 @@ static ssize_t device_read(struct file *file, /* see include/linux/fs.h   */
 
 	copy_to_user(buffer, result, sizeof(result));
 
-	#ifdef DEBUG
+	#ifdef DEBUG_DES3MODULE
 	pr_info("Read 52 bytes\n"); 
 	#endif
 	return 52;
@@ -153,14 +151,14 @@ static ssize_t device_write(struct file *file, const char __user *buffer,
 
 	// checks
 	if (length != 36) {
-		#ifdef DEBUG
+		#ifdef DEBUG_DES3MODULE
 		pr_info("lenght is %d\n", length);
 		#endif
 		pr_alert("Tried to write length != 36\nNothing done\n");
 		return 0;
 	}
 	if (*offset != 0) {
-		#ifdef DEBUG
+		#ifdef DEBUG_DES3MODULE
 		pr_info("Offset != 0\nOffset was ignored\n");
 		#endif
 	}
@@ -169,7 +167,7 @@ static ssize_t device_write(struct file *file, const char __user *buffer,
     for (int i = 0; i < length; i++) {
 		get_user(kernel_buffer[i], buffer + i); 
 	}
-	#ifdef DEBUG
+	#ifdef DEBUG_DES3MODULE
 	pr_info("Starting write to AXI Registers\n");
 	#endif
 	iowrite32(*((unsigned int*)(kernel_buffer   )), axi_slv_regs + DES3_AXI_KEY1_HI_OFFSET);
@@ -181,7 +179,7 @@ static ssize_t device_write(struct file *file, const char __user *buffer,
 	iowrite32(*((unsigned int*)(kernel_buffer+24)), axi_slv_regs + DES3_AXI_DES_IN_HI_OFFSET);
 	iowrite32(*((unsigned int*)(kernel_buffer+28)), axi_slv_regs + DES3_AXI_DES_IN_LO_OFFSET);
 	iowrite32(*((unsigned int*)(kernel_buffer+32)), axi_slv_regs + DES3_AXI_DECRYPT_OFFSET);
-	#ifdef DEBUG
+	#ifdef DEBUG_DES3MODULE
 	pr_info("Write to AXI Registers completed\n");
 	#endif
     /* Return the number of input characters used. */ 
@@ -228,7 +226,7 @@ static int __init des3module_init(void)
 		printk(KERN_INFO "failed to map axi base address\n");
 		return -ENOMEM;
 	}
-	#ifdef DEBUG
+	#ifdef DEBUG_DES3MODULE
 	pr_info("Successfully remapped axi_slv_regs from 0x%x to 0x%p", XPAR_DES3_AXI_0_S00_AXI_BASEADDR, axi_slv_regs);
 	#endif
 
@@ -242,7 +240,7 @@ static void __exit des3module_exit(void)
     class_destroy(cls); 
     /* Unregister the device */ 
     unregister_chrdev(MAJOR_NUM, DRIVER_FILE_NAME);
-	#ifdef DEBUG
+	#ifdef DEBUG_DES3MODULE
 	printk(KERN_ALERT "Goodbye module world.\n");
 	#endif
 }
